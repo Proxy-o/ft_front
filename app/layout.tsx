c6172e966176a17c6047d7f8badcc3d6537e236d
proxy <you@example.com>
2024-01-16 17:32:08 +0100

    adding dark mode

--- app/layout.tsx
+++ app/layout.tsx
@@ -9,6 +9,7 @@ const inter = Inter({ subsets: ["latin"] });
 import { Suspense, lazy } from "react";
 import NavSkel from "@/components/skeletons/navSkel";
 import HomeSkel from "@/components/skeletons/homeSkel";
+import { ThemeProvider } from "@/lib/providers/ThemeProvider";
 
 const Nav = lazy(() => import("@/components/nav"));
 
@@ -26,12 +27,19 @@ export default function RootLayout({
     <html lang="en">
       <body className={inter.className}>
         <TanstackProvider>
-          <Suspense fallback={<HomeSkel />}>
-            <div className="flex ">
-              <Nav />
-              <main>{children}</main>
-            </div>
-          </Suspense>
+          <ThemeProvider
+            attribute="class"
+            defaultTheme="dark"
+            enableSystem
+            disableTransitionOnChange
+          >
+            <Suspense fallback={<HomeSkel />}>
+              <div className="flex">
+                <Nav />
+                <main>{children}</main>
+              </div>
+            </Suspense>
+          </ThemeProvider>
         </TanstackProvider>
       </body>
     </html>
