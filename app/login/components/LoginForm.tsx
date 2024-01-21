a7f7bb6061b8cb0a989dc2b7ea4dbbd052179ada
proxy <you@example.com>
2024-01-21 12:48:55 +0100

    login page with backend

--- app/login/components/LoginForm.tsx
+++ app/login/components/LoginForm.tsx
@@ -12,51 +12,58 @@ import { Input } from "@/components/ui/input";
 import { Button } from "@/components/ui/button";
 import Link from "next/link";
 import { Separator } from "@/components/ui/separator";
-import { Asterisk, Key, User } from "lucide-react";
-import { useContext, useState } from "react";
+import { Asterisk, Key, Loader, User } from "lucide-react";
+import { useContext, useEffect, useState } from "react";
 import { UserContext } from "@/lib/providers/UserContext";
 import useLogin from "../hooks/useLogin";
-import { useMutation } from "@tanstack/react-query";
-import axios from "axios";
+import { useRouter } from "next/navigation";
 
 export default function LoginForm() {
-  const userContext = useContext(UserContext);
-  const { currentUser, setCurrentUser } = userContext;
+  const { currentUser, setCurrentUser } = useContext(UserContext);
+  const { mutate: login, isError, isPending, isSuccess, data } = useLogin();
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
+  const router = useRouter();
 
-  const mutation = useMutation({
-    mutationFn: async (formData: FormData) => {
-      const path = process.env.NEXT_PUBLIC_API_URL + "/login";
-      console.log(path);
-      const response = await axios.post(
-        process.env.NEXT_PUBLIC_API_URL + "/login",
-        formData
-      );
-      console.log(response);
-      return response;
-    },
-  });
+  useEffect(() => {
+    if (isSuccess) {
+      console.log(data);
+      setCurrentUser({
+        id: data.id,
+        username: data.username,
+        email: data.email,
+        token: data.token,
+        avatar: data.avatar,
+      });
+      router.push("/profile");
+    }
+  }, [isSuccess, data, setCurrentUser, router]);
 
-  const onSubmit = (event: any) => {
-    event.preventDefault();
-    mutation.mutate(new FormData(event.target));
+  const onSubmit = async () => {
+    login({ username, password });
   };
+
   return (
-    <form
-      className="flex flex-col items-center justify-center min-h-screen overflow-hidden"
-      onSubmit={onSubmit}
-    >
+    <div className="flex flex-col items-center justify-center min-h-screen overflow-hidden">
       <Card className="w-full max-w-md ">
         <CardHeader className="text-center">
           <CardTitle className="text-2xl">Login</CardTitle>
           <CardDescription>Enter your username and password</CardDescription>
+          {isError && (
+            <Card className="text-primary text-sm mt-2 p-2 bg-primary/10">
+              Invalid username or password
+            </Card>
+          )}
         </CardHeader>
         <CardContent className="space-y-4">
           <div className="space-y-2">
             <Label htmlFor="username">Username</Label>
             <div className="relative">
-              <Input placeholder="Username" type="text" />
+              <Input
+                placeholder="Username"
+                type="text"
+                onChange={(ev) => setUsername(ev.target.value)}
+              />
               <div className="absolute right-3 top-1/2 transform -translate-y-1/2">
                 <User className="h-5 w-5 text-secondary" />
               </div>
@@ -73,7 +80,11 @@ export default function LoginForm() {
               </Link>
             </div>
             <div className="relative">
-              <Input placeholder="Password" type="password" />
+              <Input
+                placeholder="Password"
+                type="password"
+                onChange={(ev) => setPassword(ev.target.value)}
+              />
               <div className="absolute right-3 top-1/2 transform -translate-y-1/2">
                 <Asterisk className="h-5 w-5 text-secondary" />
               </div>
@@ -82,7 +93,13 @@ export default function LoginForm() {
         </CardContent>
         <CardFooter className="flex flex-col">
           <div className="flex w-full">
-            <Button className="w-full">Login</Button>
+            <Button className="w-full" onClick={onSubmit}>
+              {isPending ? (
+                <Loader className="h-5 w-5 animate-spin animation" />
+              ) : (
+                "Login"
+              )}
+            </Button>
           </div>
           <div className="flex w-32 justify-center my-4 text-xs items-center">
             <Separator className="my-4 mr-2 " />
@@ -91,13 +108,17 @@ export default function LoginForm() {
           </div>
           <div className="flex w-full">
             <Link className="w-full" href="/register">
-              <Button className="w-full" variant="secondary" type="submit">
+              <Button
+                className="w-full"
+                variant="secondary"
+                disabled={isPending}
+              >
                 Register
               </Button>
             </Link>
           </div>
         </CardFooter>
       </Card>
-    </form>
+    </div>
   );
 }
--- app/login/hooks/useLogin.ts
+++ app/login/hooks/useLogin.ts
@@ -2,15 +2,14 @@ import { useMutation } from "@tanstack/react-query";
 import axios from "axios";
 
 // login hook
-export default function useLogin({ username, password }: { username: string, password: string }) {
+export default function useLogin() {
 
-const info = useMutation({
-mutationFn: async (formData: FormData) => {
-    console.log("login hook");
-    console.log(formData);
-    // const response = await axios.post(process.env.API_URL + "/login", formData);
-    // return response.data;
-},
-})
- return info;
+    const mutation = useMutation({
+        mutationFn: async (data: { username: string; password: string }) => {
+          const path = process.env.NEXT_PUBLIC_API_URL + "/login";
+          const response = await axios.post(path, data);
+          return response.data;
+        },
+      });
+      return mutation;
 }
