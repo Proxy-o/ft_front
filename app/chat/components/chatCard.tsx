21396144a287c21e38d4062162e4a29b0bcb8ff2
proxy <you@example.com>
2024-01-30 16:36:12 +0100

    chat messages good

--- app/chat/components/chatCard.tsx
+++ app/chat/components/chatCard.tsx
@@ -2,7 +2,7 @@
 import { Button } from "@/components/ui/button";
 import getCookie from "@/lib/functions/getCookie";
 import React, { useState, useCallback, useEffect } from "react";
-import useWebSocket, { ReadyState } from "react-use-websocket";
+import useWebSocket from "react-use-websocket";
 import { Card } from "@/components/ui/card";
 import useGetMessages from "../hooks/useGetMessages";
 import ChatBubble from "./chatBubble";
@@ -10,6 +10,7 @@ import { Conversation } from "../types";
 import { Input } from "@/components/ui/input";
 import { useQueryClient } from "@tanstack/react-query";
 import InfiniteScroll from "react-infinite-scroll-component";
+import { SendHorizonal } from "lucide-react";
 
 export default function ChatCard({ senderId, receiverId }: Conversation) {
   const token = getCookie("refresh");
@@ -19,26 +20,19 @@ export default function ChatCard({ senderId, receiverId }: Conversation) {
   );
   const { sendJsonMessage, lastMessage, readyState } = useWebSocket(socketUrl);
 
-  const connectionStatus = {
-    [ReadyState.CONNECTING]: "Connecting",
-    [ReadyState.OPEN]: "Open",
-    [ReadyState.CLOSING]: "Closing",
-    [ReadyState.CLOSED]: "Closed",
-    [ReadyState.UNINSTANTIATED]: "Uninstantiated",
-  }[readyState];
-
   const [hasMore, setHasMore] = useState(true);
   const [message, setMessage] = useState("");
   const [msgLength, setMsgLength] = useState(0);
-
-  const handelSendMessage = () => {
-    const toSend = "/pm " + receiverId + " " + message;
-    sendJsonMessage({ message: toSend });
-  };
   const { data, isSuccess, fetchNextPage } = useGetMessages({
     senderId,
     receiverId,
   });
+
+  const handelSendMessage = () => {
+    const toSend = "/pm " + receiverId + " " + message;
+    sendJsonMessage({ message: toSend });
+    setMessage("");
+  };
 
   const queryClient = useQueryClient();
   useEffect(() => {
@@ -61,53 +55,52 @@ export default function ChatCard({ senderId, receiverId }: Conversation) {
   }, [fetchNextPage, data]);
 
   return (
-    <Card className="p-2 relative ">
+    <Card className="relative ">
       <div
+        className="no-scrollbar h-[30rem] overflow-auto flex flex-col-reverse mx-2"
         id="scrollableDiv"
-        style={{
-          height: 300,
-          overflow: "auto",
-          display: "flex",
-          flexDirection: "column-reverse",
-        }}
       >
         <InfiniteScroll
+          className="flex flex-col-reverse"
           dataLength={msgLength}
           next={handleFetchNextPage}
-          style={{ display: "flex", flexDirection: "column-reverse" }} //To put endMessage and loader to the top.
-          inverse={true} //
+          inverse={true}
           hasMore={hasMore}
           loader={<h4>Loading...</h4>}
           scrollableTarget="scrollableDiv"
         >
           {isSuccess &&
-            data?.pages
-              // .slice()
-              // .reverse()
-              .map((page) => {
+            data?.pages.map((page) => {
+              return page.results.map((result: any, index: number) => {
                 return (
-                  page.results
-                    // .slice()
-                    // .reverse()
-                    .map((result: any, index: number) => {
-                      return (
-                        <ChatBubble
-                          key={index}
-                          message={result.content}
-                          me={result.user == user_id}
-                        />
-                      );
-                    })
+                  <ChatBubble
+                    key={index}
+                    message={result.content}
+                    me={result.user == user_id}
+                  />
                 );
-              })}
+              });
+            })}
         </InfiniteScroll>
       </div>
-      <div className="flex justify-between items-center">
+      <div className="flex justify-between items-center bg-secondary/30 w-full p-2 relative">
         <Input
-          className="w-full"
+          value={message}
+          className="w-full mr-2 h-12"
           onChange={(e) => setMessage(e.target.value)}
+          onKeyDown={(e) => {
+            if (e.key === "Enter") {
+              handelSendMessage();
+            }
+          }}
         />
-        <Button onClick={handelSendMessage}>Send</Button>
+        <Button
+          onClick={handelSendMessage}
+          className="absolute right-6 h-15 w-13"
+          variant={"outline"}
+        >
+          <SendHorizonal className="text-primary" size={15} />
+        </Button>
       </div>
     </Card>
   );
