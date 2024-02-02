3e11f39887f7bfedb9c22a2e95e225dda5b84128
proxy <you@example.com>
2024-02-02 11:34:28 +0100

    fix my sent friend req shows on me

--- app/friends/components/friendRequests.tsx
+++ app/friends/components/friendRequests.tsx
@@ -9,50 +9,59 @@ import { Button } from "@/components/ui/button";
 import { CheckCircle, XCircle } from "lucide-react";
 import useAcceptFriend from "../hooks/useAcceptFriend";
 import useReject from "../hooks/useDeclineReq";
+import useGetUser from "@/app/profile/hooks/useGetUser";
+import getCookie from "@/lib/functions/getCookie";
 
 export default function FriendRequests() {
   const { data, isSuccess } = useGetFrdReq();
   const { mutate: acceptFriend } = useAcceptFriend();
   const { mutate: reject } = useReject();
+  const user_id = getCookie("user_id");
+
+  const filteredData =
+    isSuccess &&
+    data.filter(({ to_user }: { to_user: User }) => to_user.id == user_id);
   return (
     isSuccess && (
       <Card className="p-2">
-        {data.map(({ id, from_user }: { id: string; from_user: User }) => (
-          <div
-            key={id}
-            className="flex hover:scale-[1.005] transition-all bg-secondary rounded-sm mb-1 p-2 "
-          >
-            <Link
-              className=" w-full text-white  flex items-center flex-1 "
-              href={`profile/${from_user.id}`}
+        {filteredData.map(
+          ({ id, from_user }: { id: string; from_user: User }) => (
+            <div
+              key={id}
+              className="flex hover:scale-[1.005] transition-all bg-secondary rounded-sm mb-1 p-2 "
             >
-              <Avatar className="rounded-sm mr-4 hover:scale-[1.1] transition-all">
-                <AvatarImage
-                  src={from_user.avatar}
-                  alt="profile image"
-                  className="rounded-sm"
-                />
-                <AvatarFallback className="rounded-sm">PF</AvatarFallback>
-              </Avatar>
-              <p className="text-zinc-700 dark:text-zinc-50">
-                {from_user.username}
-              </p>
-            </Link>
+              <Link
+                className=" w-full text-white  flex items-center flex-1 "
+                href={`profile/${from_user.id}`}
+              >
+                <Avatar className="rounded-sm mr-4 hover:scale-[1.1] transition-all">
+                  <AvatarImage
+                    src={from_user.avatar}
+                    alt="profile image"
+                    className="rounded-sm"
+                  />
+                  <AvatarFallback className="rounded-sm">PF</AvatarFallback>
+                </Avatar>
+                <p className="text-zinc-700 dark:text-zinc-50">
+                  {from_user.username}
+                </p>
+              </Link>
 
-            <div className="flex items-center">
-              <CheckCircle
-                className="text-green-500 hover:text-green-400 hover:scale-[1.1] transition-all mr-2 cursor-pointer"
-                onClick={() =>
-                  acceptFriend({ friend: from_user, to_accept_id: id })
-                }
-              />
-              <XCircle
-                className="text-red-500 hover:text-red-400 hover:scale-[1.1] transition-all  mr-2 cursor-pointer"
-                onClick={() => reject(id)}
-              />
+              <div className="flex items-center">
+                <CheckCircle
+                  className="text-green-500 hover:text-green-400 hover:scale-[1.1] transition-all mr-2 cursor-pointer"
+                  onClick={() =>
+                    acceptFriend({ friend: from_user, to_accept_id: id })
+                  }
+                />
+                <XCircle
+                  className="text-red-500 hover:text-red-400 hover:scale-[1.1] transition-all  mr-2 cursor-pointer"
+                  onClick={() => reject(id)}
+                />
+              </div>
             </div>
-          </div>
-        ))}
+          )
+        )}
       </Card>
     )
   );
