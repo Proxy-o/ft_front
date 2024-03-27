ff909f9d6492f7cbb8c04dbee0807d94a209fd3d
proxy-o <aittaleb108@gmail.com>
2024-03-27 22:49:36 +0000

    navbar

--- components/nav.tsx
+++ components/nav.tsx
@@ -36,12 +36,15 @@ interface linksProps {
   variant: "default" | "ghost";
 }
 
-let lastInvitation:MessageEvent<any> | null = null;
+let lastInvitation: MessageEvent<any> | null = null;
 export default function Nav() {
   const { mutate: logout } = useLogout();
-  
-  const {newNotif} = useGameSocket();
+  const { theme, setTheme } = useTheme();
+  const { newNotif } = useGameSocket();
   const [notification, setNotification] = useState(false);
+  const isCollapsed = useMediaQuery("(max-width: 768px)");
+  const path = usePathname();
+
   const links: linksProps[] = [
     {
       title: "login",
@@ -80,16 +83,13 @@ export default function Nav() {
       variant: "ghost",
     },
   ];
-  const { theme, setTheme } = useTheme();
-  const isCollapsed = useMediaQuery("(max-width: 768px)");
-  const path = usePathname();
 
   const activeLink = links.findIndex((link) => link.link === path);
-  // set the variant of the active link to default
   links[activeLink] = {
     ...links[activeLink],
     variant: "default",
   };
+
   const handleLogout = () => {
     logout();
   };
@@ -102,8 +102,8 @@ export default function Nav() {
     if (newNotif()) {
       setNotification(true);
       const notif = newNotif();
-        const message = notif && JSON.parse(notif?.data) || "";
-        if (message && message.message?.split(" ")[0] === "/notif") {
+      const message = (notif && JSON.parse(notif?.data)) || "";
+      if (message && message.message?.split(" ")[0] === "/notif") {
         toast.info("you have a new invitation");
         lastInvitation = newNotif();
       }
@@ -113,182 +113,75 @@ export default function Nav() {
   return (
     <div
       data-collapsed={isCollapsed}
-      className="group flex flex-col gap-4 py-2 h-screen shadow-lg md:w-[8.5rem]"
+      className="group flex flex-col gap-4 py-2 h-screen shadow-lg md:w-[8.5rem] "
     >
       <nav className="flex flex-col gap-1 px-2 h-full ">
-        {links.map((link, index) =>
-          isCollapsed ? (
-            <TooltipProvider key={index} delayDuration={0}>
-              <Tooltip key={index} delayDuration={0}>
-                <TooltipTrigger asChild>
-                  <Link
-                    href={link.link}
-                    className={cn(
-                      buttonVariants({ variant: link.variant, size: "icon" }),
-                      "h-10 w-10 mb-2 "
-                    )}
-                  >
-                    {link.title === "chat" && lastMessage ? (
-                      <div className="relative">
-                        <span className="h-2 w-2 bg-primary rounded-full absolute top-0 right-0 "></span>
-                        <link.icon className=" h-6 w-6 " />
-                      </div>
-                    ) : (
-                      <link.icon className=" h-6 w-6 " />
-                    )}
-                    <span className="sr-only">{link.title}</span>
-                  </Link>
-                </TooltipTrigger>
-                <TooltipContent
-                  side="right"
-                  className="flex items-center gap-4"
-                >
-                  {link.title}
-                </TooltipContent>
-              </Tooltip>
-            </TooltipProvider>
-          ) : (
-            <Link
-              key={index}
-              href={link.link}
-              className={cn(
-                buttonVariants({ variant: link.variant, size: "sm" }),
-
-                "justify-start mb-2 "
-              )}
-            >
-              {link.title === "chat" && lastMessage ? (
-                <div className="relative">
-                  <span className="h-2 w-2 bg-primary rounded-full absolute top-0 right-0 "></span>
-                  <link.icon className="mr-2 h-6 w-6 " />
-                </div>
-              ) : (
-                (link.title === "Play" && notification) ? (
-                  <div className="relative">
-                    <link.icon className=" h-6 w-6 " />
-                    <span className="h-3 w-3 bg-white rounded-full absolute top-0 right-0 "></span>
-                    <span className="h-1 w-1 bg-primary rounded-full absolute top-1 right-1 "></span>
-                  </div>
-                  ) : (
-                    <link.icon className=" h-6 w-6 " />
-                  )
-              )}
-              {link.title}
-            </Link>
-          )
-        )}
-      </nav>
-      {isCollapsed ? (
-        <div className="flex flex-col gap-1 px-2  ">
-          <TooltipProvider delayDuration={0}>
-            <Tooltip delayDuration={0}>
-              <TooltipTrigger asChild>
-                <Link
-                  href="#"
-                  className={cn(
-                    buttonVariants({ variant: "ghost", size: "icon" }),
-                    "h-9 w-9 mb-2"
-                  )}
-                >
-                  <UserRoundCog className="h-6 w-6" />
-                  <span className="sr-only">Sittings</span>
-                </Link>
-              </TooltipTrigger>
-              <TooltipContent side="right" className="flex items-center gap-4">
-                Sittings
-              </TooltipContent>
-            </Tooltip>
-          </TooltipProvider>
-          <TooltipProvider delayDuration={0}>
-            <Tooltip delayDuration={0}>
-              <TooltipTrigger asChild>
-                <Link
-                  href="#"
-                  className={cn(
-                    buttonVariants({ variant: "ghost", size: "icon" }),
-                    "h-9 w-9 mb-2"
-                  )}
-                >
-                  <LogOut className="h-6 w-6" />
-                  <span className="sr-only ">Logout</span>
-                </Link>
-              </TooltipTrigger>
-              <TooltipContent side="right" className="flex items-center gap-4">
-                Logout
-              </TooltipContent>
-            </Tooltip>
-            <TooltipProvider delayDuration={0}>
-              <Tooltip delayDuration={0}>
-                <TooltipTrigger asChild>
-                  <button
-                    onClick={() =>
-                      setTheme(theme === "dark" ? "light" : "dark")
-                    }
-                    className={cn(
-                      buttonVariants({ variant: "ghost", size: "icon" }),
-                      "h-9 w-9 mb-2"
-                    )}
-                  >
-                    {theme === "dark" ? (
-                      <Sun className="h-6 w-6" />
-                    ) : (
-                      <Moon className="h-6 w-6" />
-                    )}
-                    <span className="sr-only">
-                      {theme === "dark" ? "Light" : "Dark"}
-                    </span>
-                  </button>
-                </TooltipTrigger>
-                <TooltipContent
-                  side="right"
-                  className="flex items-center gap-4"
-                >
-                  {theme === "dark" ? "Light" : "Dark"}
-                </TooltipContent>
-              </Tooltip>
-            </TooltipProvider>
-          </TooltipProvider>
-        </div>
-      ) : (
-        <div className="flex flex-col px-2">
+        {links.map((link, index) => (
           <Link
-            href="#"
+            key={index}
+            href={link.link}
             className={cn(
-              buttonVariants({ variant: "ghost", size: "sm" }),
+              buttonVariants({ variant: link.variant, size: "sm" }),
+
               "justify-start mb-2"
             )}
           >
-            <UserRoundCog className="mr-2 h-6 w-6 " />
-            Sittings
-          </Link>
-          <Button
-            variant={"ghost"}
-            className={cn(
-              buttonVariants({ variant: "ghost", size: "sm" }),
-              "justify-start mb-2"
-            )}
-            onClick={() => handleLogout()}
-          >
-            <LogOut className="mr-2 h-6 w-6 " />
-            logout
-          </Button>
-          {/* add shose them them */}
-          <button
-            onClick={() => setTheme(theme === "dark" ? "light" : "dark")}
-            className={cn(
-              buttonVariants({ variant: "ghost", size: "sm" }),
-              "justify-start text-primary mb-2"
-            )}
-          >
-            {theme === "dark" ? (
-              <Sun className="mr-2 h-6 w-6 " />
+            {link.title === "chat" && lastMessage ? (
+              <div className="relative">
+                <span className="h-2 w-2 bg-primary rounded-full absolute top-0 right-0 "></span>
+                <link.icon className="mr-2 h-6 w-6 " />
+              </div>
+            ) : link.title === "Play" && notification ? (
+              <div className="relative">
+                <link.icon className=" h-6 w-6 " />
+                <span className="h-3 w-3 bg-white rounded-full absolute top-0 right-0 "></span>
+                <span className="h-1 w-1 bg-primary rounded-full absolute top-1 right-1 "></span>
+              </div>
             ) : (
-              <Moon className="mr-2 h-6 w-6 " />
+              <link.icon className=" mr-2 h-6 w-6 " />
             )}
-            {theme === "dark" ? "Light" : "Dark"}
-          </button>
-        </div>
-      )}
+            {link.title}
+          </Link>
+        ))}
+      </nav>
+
+      <div className="flex flex-col px-2">
+        <Link
+          href="#"
+          className={cn(
+            buttonVariants({ variant: "ghost", size: "sm" }),
+            "justify-start mb-2"
+          )}
+        >
+          <UserRoundCog className="mr-2 h-6 w-6 " />
+          Sittings
+        </Link>
+        <Button
+          variant={"ghost"}
+          className={cn(
+            buttonVariants({ variant: "ghost", size: "sm" }),
+            "justify-start mb-2"
+          )}
+          onClick={() => handleLogout()}
+        >
+          <LogOut className="mr-2 h-6 w-6 " />
+          logout
+        </Button>
+        <button
+          onClick={() => setTheme(theme === "dark" ? "light" : "dark")}
+          className={cn(
+            buttonVariants({ variant: "ghost", size: "sm" }),
+            "justify-start text-primary mb-2"
+          )}
+        >
+          {theme === "dark" ? (
+            <Sun className="mr-2 h-6 w-6 " />
+          ) : (
+            <Moon className="mr-2 h-6 w-6 " />
+          )}
+          {theme === "dark" ? "Light" : "Dark"}
+        </button>
+      </div>
     </div>
   );
 }
