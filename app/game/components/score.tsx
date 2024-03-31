4e6022f81a2d9d487dff10248c760d4e15486b4f
hamza nait <hamza2nait@gmail.com>
2024-03-31 21:34:46 +0000

    UI: Responsive

--- app/game/components/score.tsx
+++ app/game/components/score.tsx
@@ -1,16 +1,26 @@
-import React from 'react'
+import React from "react";
 
-export default function Score({leftPlayerScore, rightPlayerScore}: {leftPlayerScore: number, rightPlayerScore: number}) {
+export default function Score({
+  leftPlayerScore,
+  rightPlayerScore,
+}: {
+  leftPlayerScore: number;
+  rightPlayerScore: number;
+}) {
   return (
-    <div className="flex flex-row justify-between items-center w-full h-16">
+    <div className="flex flex-row justify-between items-center w-5/6 h-16 mx-auto">
       <div className="flex flex-row justify-start items-center w-1/2">
-        <div className="w-6 h-6 bg-white rounded-full flex justify-center items-center text-black font-bold">{leftPlayerScore}</div>
+        <div className="w-6 h-6 bg-white rounded-full flex justify-center items-center text-black font-bold">
+          {leftPlayerScore}
+        </div>
         <div className="text-white ml-2">Player 1</div>
       </div>
       <div className="flex flex-row justify-end items-center w-1/2">
         <div className="text-white mr-2">Player 2</div>
-        <div className="w-6 h-6 bg-white rounded-full flex justify-center items-center text-black font-bold">{rightPlayerScore}</div>
+        <div className="w-6 h-6 bg-white rounded-full flex justify-center items-center text-black font-bold">
+          {rightPlayerScore}
+        </div>
       </div>
     </div>
-  )
+  );
 }
--- app/game/page.tsx
+++ app/game/page.tsx
@@ -11,42 +11,44 @@ import GameNav from "./components/gameNav";
 import OneOnline from "./components/oneOnline";
 import OneOffline from "./components/oneOffline";
 export default function Page() {
-    const user_id = getCookie("user_id") || "";
-    const { data: user, isSuccess } = useGetUser(user_id || "0");
-    const router = useRouter();
-    const [tab, setTab] = useState("local");
-    
-    useEffect(() => {
-        if (!user && isSuccess) {
-            router.push("/login");
-        }
-    }, [user, isSuccess, router]);
+  const user_id = getCookie("user_id") || "";
+  const { data: user, isSuccess, isLoading } = useGetUser(user_id || "0");
+  const router = useRouter();
+  const [tab, setTab] = useState("online");
 
-    // window.addEventListener('offline', () => {
-    //     console.log("offline");
-    // }
-    // );
+  useEffect(() => {
+    if (!user && isSuccess) {
+      router.push("/login");
+    }
+  }, [user, isSuccess, router]);
 
-    return (
-        <>
-            <GameNav setTab={setTab} tab={tab} />
-            <div className="w-[500px] h-fit flex flex-row justify-start items-start dark:text-white mx-auto mt-10">
-                {(tab === "online") && (
-                <>
-                    <div className="w-fit h-fit flex flex-col justify-start items-start dark:text-white">
-                        <Invitations />
-                        <Separator className="w-full mt-4" />
-                        <InviteFriend />
-                    </div>
-                    <OneOnline />
-                </>
-                )}
-                {(tab === "local") && (
-                    <>
-                        <OneOffline />
-                    </>
-                )}
+  if (isLoading) return <div>loading...</div>;
+
+  // window.addEventListener('offline', () => {
+  //     console.log("offline");
+  // }
+  // );
+
+  return (
+    <>
+      <GameNav setTab={setTab} tab={tab} />
+      <div className="w-full h-fit flex flex-row justify-start bg-cyan-600 items-start dark:text-white mx-auto mt-10">
+        {tab === "online" && (
+          <div className="flex flex-col w-full h-full justify-start items-center">
+            <div className="w-fit h-fit flex flex-col justify-start items-start dark:text-white mb-9">
+              <Invitations />
+              <Separator className="w-full mt-4" />
+              <InviteFriend />
             </div>
-        </>
-    );
+            <OneOnline />
+          </div>
+        )}
+        {tab === "local" && (
+          <>
+            <OneOffline />
+          </>
+        )}
+      </div>
+    </>
+  );
 }
