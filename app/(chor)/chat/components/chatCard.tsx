a63434f6b0d44b249ffc23b17457311da23b10cc
proxy-o <aittaleb108@gmail.com>
2024-06-01 19:47:41 +0100

    refactor: Update chatCard.tsx to handle blocked messages

--- app/(chor)/chat/components/chatCard.tsx
+++ app/(chor)/chat/components/chatCard.tsx
@@ -64,7 +64,7 @@ export default function ChatCard({
     }
     if (
       lastJsonMessage &&
-      (lastJsonMessage as { type: string }).type !== "private_message_delivered"
+      (lastJsonMessage as { type: string }).type === "blocked"
     ) {
       setIsBlocked(true);
     }
