0b0899097ebcaf4658cf54e0ebbdd090e0b5d2a2
proxy-o <aittaleb108@gmail.com>
2024-06-28 19:21:12 +0100

    chore: Update backend and chat URLs to use secure connections
    ```

--- app/(chor)/profile/settings/page.tsx
+++ app/(chor)/profile/settings/page.tsx
@@ -3,9 +3,9 @@ import React from "react";
 import useToggleOTP from "./hooks/useToggleOTP";
 import useGetUser from "../hooks/useGetUser";
 import getCookie from "@/lib/functions/getCookie";
-import Image from "next/image";
 import { Button } from "@/components/ui/button";
 import EditProfileForm from "../components/editProfileForm";
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
 
 export default function Page() {
   const userId = getCookie("user_id");
@@ -14,14 +14,15 @@ export default function Page() {
   return (
     <div className="gap-4 w-full h-full flex flex-col justify-center items-center">
       {isSuccess && user.otp_active && (
-        <div>
-          <Image
-            src={user?.qr_code ?? ""}
-            alt="avatar"
-            width={230}
-            height={230}
-          />
-        </div>
+
+              <Avatar className=" mr-2 size-48">
+              <AvatarImage
+                src={user?.qr_code}
+                alt="QR Code"
+                className="rounded-sm size-48"
+              />
+              <AvatarFallback className="rounded-sm size-4 text-xs">QR</AvatarFallback>
+            </Avatar>
       )}
       <div className="space-x-3">
         <Button disabled={user?.otp_active} onClick={() => toggleOTP("enable")}>
