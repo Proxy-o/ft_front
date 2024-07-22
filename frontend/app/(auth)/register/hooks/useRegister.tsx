403ba44985237a1e382ea488da4e33ecc5313e81
proxy-o <aittaleb108@gmail.com>
2024-07-22 11:20:26 +0100

    refactor: Update Docker Compose to mount frontend code directory and prevent overwriting node_modules

--- frontend/app/(auth)/register/hooks/useRegister.tsx
+++ frontend/app/(auth)/register/hooks/useRegister.tsx
@@ -12,6 +12,7 @@ export default function useRegister() {
       password: string;
     }) => {
       const response = await axiosInstance.post("/signup", data);
+      console.log(response.data);
       return response.data;
     },
     onSuccess: (data) => {
@@ -23,8 +24,8 @@ export default function useRegister() {
         toast.error((error as any).response.data.username);
       } else if ((error as any).response.data.email) {
         toast.error((error as any).response.data.email);
-      } else {
-        toast.error("Error creating account");
+      } else if ((error as any).response.data.password) {
+        toast.error((error as any).response.data.password);
       }
     },
   });
