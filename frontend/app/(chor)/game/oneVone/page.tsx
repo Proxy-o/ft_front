450302125543d532050b6790fb670fe2469cd25b
tianitoo <hamza2nait@gmail.com>
2024-08-04 18:55:10 +0100

    [UI]: 1VS1 perfct

--- frontend/app/(chor)/game/oneVone/page.tsx
+++ frontend/app/(chor)/game/oneVone/page.tsx
@@ -6,13 +6,13 @@ import InviteFriends from "../components/inviteFriend";
 export default function Page() {
   return (
     <>
-      <div className="flex flex-col p-4 lg:flex-row w-full h-full justify-center items-start gap-2 top-0">
+      <div className="flex flex-col p-4 lg:flex-row w-full h-full lg:justify-center items-center lg:items-start gap-2">
         <Card className="w-11/12 max-w-[900px] h-fit flex flex-col justify-center items-start gap-4 p-4">
           <div className="w-full h-fit">
             <Two type="two" />
           </div>
         </Card>
-        <div className="w-11/12 lg:w-4/12 h-fit flex flex-col justify-start items-start gap-2">
+        <div className="w-11/12 lg:w-4/12 lg:max-w-96 h-fit flex flex-col justify-start items-start gap-2">
           <div className="w-full h-full flex flex-col justify-start items-start">
             <Invitations mode="two" />
           </div>
