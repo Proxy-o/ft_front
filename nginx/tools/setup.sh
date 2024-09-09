c5b9abbd6a5b7bc5c730a6c3757cf127710d6ed3
ans <anassnammal@gmail.com>
2024-09-09 10:47:26 +0100

    Refactor Makefile and Dockerfile for better organization and functionality

--- nginx/tools/setup.sh
+++ nginx/tools/setup.sh
@@ -2,7 +2,24 @@
 
 set -e
 
-# init setup
+append_or_replace() {
+    local file=$1
+    local pattern=$2
+    local text=$3
+
+    # Escape special characters in pattern and text
+    local escaped_pattern=$(printf '%s\n' "$pattern" | sed 's/[]\/$*.^|[]/\\&/g')
+    local escaped_text=$(printf '%s\n' "$text" | sed 's/[&/\]/\\&/g')
+
+    if grep -q "$escaped_pattern" "$file"; then
+        sed -i "s|$escaped_pattern|$escaped_text|g" "$file"
+    else
+        echo "$text" >> "$file"
+    fi
+}
+
+
+# init setupSecAuditLog
 SETUP_DEPS=(
     "openssl"
     "wget"
@@ -23,13 +40,28 @@ openssl req -x509 -nodes -days 365 -newkey rsa:2048 \
 rm -f /etc/nginx/conf.d/default.conf
 mv nginx.conf /etc/nginx/nginx.conf
 
+# set the log directory
+mkdir -p /var/log/nginx
+chmod 755 /var/log/nginx
+
 # integrate modSecurity config
 mkdir -p /etc/nginx/modsec
 wget https://raw.githubusercontent.com/SpiderLabs/ModSecurity/v3/master/modsecurity.conf-recommended -O /etc/nginx/modsec/modsecurity.conf
+# Enable ModSecurity
+append_or_replace /etc/nginx/modsec/modsecurity.conf "^SecRuleEngine" "SecRuleEngine On"
+# Enable logging
+append_or_replace /etc/nginx/modsec/modsecurity.conf "^SecAuditEngine" "SecAuditEngine On"
+append_or_replace /etc/nginx/modsec/modsecurity.conf "^SecAuditLogRelevantStatus" "SecAuditLogRelevantStatus \"^(?:5|4(?!04))\""
+append_or_replace /etc/nginx/modsec/modsecurity.conf "^SecAuditLogParts" "SecAuditLogParts ABIJDEFHZ"
+append_or_replace /etc/nginx/modsec/modsecurity.conf "^SecAuditLogType" "SecAuditLogType Serial"
+append_or_replace /etc/nginx/modsec/modsecurity.conf "^SecAuditLog" "SecAuditLog /var/log/nginx/modsec_audit.log"
+# Set the debug log level (0-9)
+append_or_replace /etc/nginx/modsec/modsecurity.conf "^SecDebugLog" "SecDebugLog /var/log/nginx/modsec_debug.log"
+append_or_replace /etc/nginx/modsec/modsecurity.conf "^SecDebugLogLevel" "SecDebugLogLevel 3"
+
 wget https://raw.githubusercontent.com/SpiderLabs/ModSecurity/v3/master/unicode.mapping -O /etc/nginx/modsec/unicode.mapping
 
 # install OWASP ModSecurity Core Rule Set
-
 wget https://github.com/coreruleset/coreruleset/archive/refs/tags/v4.6.0.tar.gz
 tar -xvf v4.6.0.tar.gz
 mkdir -p /etc/nginx/modsec/owasp-crs
@@ -45,5 +77,3 @@ echo "Include /etc/nginx/modsec/owasp-crs/rules/*.conf" >> /etc/nginx/modsec/mod
 apt-get purge -y \
     ${SETUP_DEPS[@]} \
     && apt-get autoremove -y && apt-get clean && rm -rf /var/lib/apt/lists/*
-
-# rm -f $0 # bey bey
