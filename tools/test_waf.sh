f4b60afbda77be73c468645e4794e7d4afae32d6
ans <anassnammal@gmail.com>
2024-10-13 18:55:46 +0100

    refactor: Update test_waf.sh script to run in bash v3

--- tools/test_waf.sh
+++ tools/test_waf.sh
@@ -1,32 +1,33 @@
 #!/bin/bash
 
-# Define the NGINX container name
+# set -x
+
+# vars
+IPADDR=$(grep "SERVER_HOST" .env | cut -d '=' -f 2)
 NGINX_CONTAINER="nginx_container"
-
-# Define the test requests
-declare -A TEST_REQUESTS
-TEST_REQUESTS["SQL Injection"]="curl -k 'https://localhost/?id=1' OR '1'='1'"
-TEST_REQUESTS["XSS"]="curl -k 'https://localhost/?q=<script>alert(\"XSS\")</script>'"
-TEST_REQUESTS["Web Scanner"]="curl -k -H 'User-Agent: Nikto' https://localhost"
-
-# Send test requests
-for TEST in "${!TEST_REQUESTS[@]}"; do
-  echo "Sending $TEST test request..."
-  eval "${TEST_REQUESTS[$TEST]}"
-done
-
-# Check ModSecurity logs
-echo "Checking ModSecurity logs..."
-
-# Define the log files
 AUDIT_LOG="/var/log/nginx/modsec_audit.log"
 DEBUG_LOG="/var/log/nginx/modsec_debug.log"
+TEST_NAMES=("SQL Injection" "XSS" "Web Scanner")
+TEST_REQUESTS=("curl -k 'https://$IPADDR/?id=1%27%20OR%20%271%27=%271'"
+               "curl -k 'https://$IPADDR/?q%3D%3Cscript%3Ealert%28%5C%22XSS%5C%22%29%3C%2Fscript%3E'"
+               "curl -k -H 'User-Agent: Nikto' https://$IPADDR")
+
+# Send test requests
+for i in "${!TEST_NAMES[@]}"; do
+  echo "Sending ${TEST_NAMES[$i]} test request..."
+  eval "${TEST_REQUESTS[$i]}"
+done
+
+# check ModSecurity logs
+echo "Checking ModSecurity logs..."
 
 # Function to check logs
 check_logs() {
   local log_file=$1
+  echo "-----------------------------------"
   echo "Checking $log_file..."
-  docker exec -it $NGINX_CONTAINER tail -n 20 $log_file
+  docker exec -it $NGINX_CONTAINER tail -n 40 $log_file
+  echo "-----------------------------------"
 }
 
 # Check audit log
