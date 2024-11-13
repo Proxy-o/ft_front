a3c260386852efe11d6274e5eaa28e83d5baf405
Anass Nammal <anassnammal@gmail.com>
2024-11-13 19:49:50 +0100

    Update: refactor settings style

--- frontend/app/(chor)/profile/components/editProfileForm.tsx
+++ frontend/app/(chor)/profile/components/editProfileForm.tsx
@@ -6,105 +6,156 @@ import {
   CardFooter,
   CardHeader,
 } from "@/components/ui/card";
-import { Dialog, DialogClose } from "@/components/ui/dialog";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
 import { User } from "@/lib/types";
 import { cn } from "@/lib/utils";
 import React, { useState } from "react";
 import useEditUser from "../hooks/useEditUser";
+import { Switch } from "@/components/ui/switch";
+import {
+  Dialog,
+  DialogContent,
+  DialogHeader,
+  DialogTrigger,
+  DialogClose,
+} from "@/components/ui/dialog"
+import { QrCode } from 'lucide-react';
+import useToggleOTP from "../settings/hooks/useToggleOTP";
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
 
 export default function EditProfileForm({ user }: { user: User }) {
   const [userInfo, setUserInfo] = useState<User>(user);
   const { mutate: editUser } = useEditUser();
+  const { mutate: toggleOTP, isSuccess: switched } = useToggleOTP("0");
+
   return (
     <Dialog>
-      <Card className="bg-none w-full max-w-lg">
+      <Card className="bg-none w-full">
         <CardHeader className="text-center">Edit profile</CardHeader>
-        <CardContent className="space-y-4">
-          <div className="space-y-2">
-            <Label htmlFor="username">Username</Label>
-            <Input
-              defaultValue={user.username}
-              id="username"
-              placeholder="Enter your username"
-              onChange={(e) =>
-                setUserInfo({
-                  ...userInfo,
-                  username: e.target.value,
-                })
-              }
-            />
+        <br />
+        <CardContent className="flex flex-col  justify-center gap-3  w-full">
+          <div className="flex flex-wrap md:flex-nowrap gap-3">
+            <div className="space-y-2 w-full">
+              <Label htmlFor="first-name">First name</Label>
+              <Input
+                defaultValue={user.first_name}
+                id="first-name"
+                placeholder="Enter your first name"
+                onChange={(e) =>
+                  setUserInfo({
+                    ...userInfo,
+                    first_name: e.target.value,
+                  })
+                }
+              />
+            </div>
+            <div className="space-y-2 w-full">
+              <Label htmlFor="last-name">Last name</Label>
+              <Input
+                defaultValue={user.last_name}
+                id="last-name"
+                placeholder="Enter your last name"
+                onChange={(e) =>
+                  setUserInfo({
+                    ...userInfo,
+                    last_name: e.target.value,
+                  })
+                }
+              />
+            </div>
           </div>
-          <div className="space-y-2">
-            <Label htmlFor="first-name">First name</Label>
-            <Input
-              defaultValue={user.first_name}
-              id="first-name"
-              placeholder="Enter your first name"
-              onChange={(e) =>
-                setUserInfo({
-                  ...userInfo,
-                  first_name: e.target.value,
-                })
-              }
-            />
+          <div className="flex flex-wrap md:flex-nowrap gap-3">
+            <div className="space-y-2 w-full">
+                <Label htmlFor="username">Username</Label>
+                <Input
+                  defaultValue={user.username}
+                  id="username"
+                  placeholder="Enter your username"
+                  onChange={(e) =>
+                    setUserInfo({
+                      ...userInfo,
+                      username: e.target.value,
+                    })
+                  }
+                />
+            </div>
+            <div className="space-y-2 w-full">
+              <Label htmlFor="email">Email</Label>
+              <Input
+                defaultValue={user.email}
+                id="email"
+                placeholder="Enter your email"
+                type="email"
+                onChange={(e) =>
+                  setUserInfo({
+                    ...userInfo,
+                    email: e.target.value,
+                  })
+                }
+              />
+            </div>
           </div>
-          <div className="space-y-2">
-            <Label htmlFor="last-name">Last name</Label>
-            <Input
-              defaultValue={user.last_name}
-              id="last-name"
-              placeholder="Enter your last name"
-              onChange={(e) =>
-                setUserInfo({
-                  ...userInfo,
-                  last_name: e.target.value,
-                })
-              }
-            />
-          </div>
-          <div className="space-y-2">
-            <Label htmlFor="email">Email</Label>
-            <Input
-              defaultValue={user.email}
-              id="email"
-              placeholder="Enter your email"
-              type="email"
-              onChange={(e) =>
-                setUserInfo({
-                  ...userInfo,
-                  email: e.target.value,
-                })
-              }
-            />
-          </div>
-          <div className="space-y-2">
-            <Label htmlFor="email">password</Label>
-            <Input
-              defaultValue={user.password}
-              id="password"
-              placeholder="Enter Your New Password"
-              type="password"
-              onChange={(e) =>
-                setUserInfo({
-                  ...userInfo,
-                  password: e.target.value,
-                })
-              }
-            />
+          <div className="flex flex-wrap md:flex-nowrap gap-3">
+            <div className="space-y-2 w-full">
+              <Label htmlFor="email">password</Label>
+              <Input
+                defaultValue={user.password}
+                id="password"
+                placeholder="Enter Your New Password"
+                type="password"
+                onChange={(e) =>
+                  setUserInfo({
+                    ...userInfo,
+                    password: e.target.value,
+                  })
+                }
+              />
+            </div>
+            <div className="space-y-2 w-full">
+              <Label htmlFor="email">One Time Passsword</Label>
+              <div className="flex justify-between items-center w-full h-10 px-2 rounded-lg border">
+                <div className="flex space-x-2">
+                  <p>{user.otp_active ? "Enabled" : "Disabled"}</p>
+                  {user && user.otp_active && (
+                    <Dialog defaultOpen={switched}>
+                        <DialogTrigger className="flex items-center space-x-2 ">
+                          <QrCode className="min-h-2 min-w-2 hover:bg-primary"></QrCode>
+                        </DialogTrigger>
+                      <DialogContent className="size-fit overflow-hidden">
+                        <DialogHeader className="m-auto">
+                          Scan the QRCode
+                        </DialogHeader>
+                        <Avatar className="m-auto size-36 sm:size-48 md:size-64">
+                          <AvatarImage
+                            src={user?.qr_code}
+                            alt="QR Code"
+                            className="rounded-sm size-36 sm:size-48 md:size-64"
+                          />
+                          <AvatarFallback className="rounded-sm size-4 text-xs">QR</AvatarFallback>
+                        </Avatar>
+                      </DialogContent>
+                    </Dialog>
+                  )}
+                </div>
+                <Switch
+                  defaultChecked={user?.otp_active}
+                  onCheckedChange={(checked: boolean) => toggleOTP(checked ? "enable" : "disable")}
+                />
+              </div>
+            </div>
           </div>
         </CardContent>
         <CardFooter>
-          <DialogClose
+        <DialogClose
             className={cn(
               buttonVariants({ variant: "default", size: "sm" }),
-              "ml-auto"
+              "ml-auto w-32"
             )}
             onClick={() => editUser(userInfo)}
           >
             Save
-          </DialogClose>
+        </DialogClose>
         </CardFooter>
       </Card>
     </Dialog>
--- frontend/app/(chor)/profile/settings/page.tsx
+++ frontend/app/(chor)/profile/settings/page.tsx
@@ -1,56 +1,17 @@
 "use client";
 import React from "react";
-import useToggleOTP from "./hooks/useToggleOTP";
 import useGetUser from "../hooks/useGetUser";
 import EditProfileForm from "../components/editProfileForm";
-import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
-import { Card } from "@/components/ui/card";
-import { Switch } from "@/components/ui/switch";
-import {
-  Dialog,
-  DialogContent,
-  DialogHeader,
-  DialogTrigger,
-} from "@/components/ui/dialog"
-import { QrCode } from 'lucide-react';
 
 export default function Page() {
   const { data: user, isSuccess } = useGetUser("0");
-  const { mutate: toggleOTP, isSuccess: switched } = useToggleOTP( "0");
+
   
   return (
-    <Card className="gap-4 h-[calc(100vh-7.8rem)] max-w-[60rem] flex flex-col items-center p-4 mx-2 overflow-auto md:scrollbar scrollbar-thumb-primary/10 scrollbar-w-2 no-scrollbar">
-      <Card className="flex justify-between items-center max-h-[200px] min-h-[64px] max-w-lg w-full px-4 overflow-x-auto no-scrollbar">
-        <div className="flex justify-start space-x-2">
-          <p>One Time Passsword</p>
-          {isSuccess && user && user.otp_active && (
-            
-            <Dialog defaultOpen={switched}>
-                <DialogTrigger className="flex items-center space-x-2 ">
-                  <QrCode className="min-h-2 min-w-2 hover:bg-primary"></QrCode>
-                </DialogTrigger>
-              <DialogContent className="size-fit overflow-hidden">
-                <DialogHeader className="m-auto">
-                  Scan the QRCode
-                </DialogHeader>
-                <Avatar className="m-auto size-36 sm:size-48 md:size-64">
-                  <AvatarImage
-                    src={user?.qr_code}
-                    alt="QR Code"
-                    className="rounded-sm size-36 sm:size-48 md:size-64"
-                  />
-                  <AvatarFallback className="rounded-sm size-4 text-xs">QR</AvatarFallback>
-                </Avatar>
-              </DialogContent>
-            </Dialog>
-          )}
-        </div>
-        <Switch
-          defaultChecked={user?.otp_active}
-          onCheckedChange={(checked: boolean) => toggleOTP(checked ? "enable" : "disable")}
-        />
-      </Card>
+    <div className="gap-4 max-h-[calc(100vh-7.8rem)] w-full flex flex-col items-center p-4 mx-2 overflow-auto md:scrollbar scrollbar-thumb-primary/10 scrollbar-w-2 no-scrollbar">
       {isSuccess && <EditProfileForm user={user} />}
-    </Card>
+
+      
+    </div>
   );
 }
