a75409eaaf452fa9cad049225c127a04d59895da
Anass Nammal <anassnammal@gmail.com>
2024-11-16 01:37:02 +0100

    chore: Add useGetGlobalState hook for fetching global game states

---
+++ frontend/app/(chor)/ranking/hooks/useGetGlobalState.ts
@@ -0,0 +1,22 @@
+import axiosInstance from "@/lib/functions/axiosInstance";
+import getCookie from "@/lib/functions/getCookie";
+import { useQuery } from "@tanstack/react-query";
+
+async function fetchGlobalState() {
+  const logged_in = getCookie("logged_in") === "yes";
+  if (!logged_in) {
+    return [];
+  }
+  const response = await axiosInstance.get(`/game/globalstates`);
+  console.log(response.data);
+  return response.data;
+}
+
+export default function useGetGlobalState() {
+  const queryKey = ["globalstates"];
+  const info = useQuery({
+    queryKey,
+    queryFn: fetchGlobalState,
+  });
+  return info;
+}
